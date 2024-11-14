74641d1a1166f326a8318f00d3adf11fc62d14e6
Ken Snyder <ken@ken.net>
2024-11-14 09:21:03 -0800

    chore: last commit on m1 max

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { useQuery } from '@triplit/vue'
+import { triplit } from '@/lib/client'
+import GettingStarted from '@/components/GettingStarted.vue'
+import ConnectionStatus from '@/components/ConnectionStatus.vue'
+import Todo from '@/components/Todo.vue'
+import { ref } from 'vue'
+
+let text = ref('')
+const { results, fetching } = useQuery(triplit, triplit.query('todos').order('created_at', 'DESC'))
+function onSubmit() {
+  triplit.insert('todos', { text: text.value })
+  text.value = ''
+}
+</script>
+
+<template>
+  <main class="main-container">
+    <GettingStarted />
+    <div class="app-container">
+      <h1>Todos</h1>
+      <ConnectionStatus />
+      <form @submit.prevent="onSubmit">
+        <input v-model="text" placeholder="What needs to be done?" class="todo-input" />
+        <button class="btn" type="submit" :disabled="!text">Add Todo</button>
+      </form>
+      <p v-if="fetching">Loading...</p>
+      <div v-if="results" class="todos-container">
+        <Todo
+          v-for="todo in results"
+          v-bind="todo.id"
+          :id="todo.id"
+          :text="todo.text"
+          :completed="todo.completed"
+          :created_at="todo.created_at"
+        />
+      </div>
+    </div>
+  </main>
+</template>
---
+++ src/components/Todo.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { triplit } from '../lib/client'
+
+const props = defineProps<{ id: string; completed: boolean; text: string }>()
+
+function toggleStatus(id: string) {
+  triplit.update('todos', id, (todo) => {
+    todo.completed = !todo.completed
+  })
+}
+
+function deleteTodo(id: string) {
+  triplit.delete('todos', id)
+}
+</script>
+
+<template>
+  <div class="todo">
+    <input @click="toggleStatus(props.id)" type="checkbox" :checked="props.completed" />
+    {{ props.text }}
+    <button class="x-button" @click="deleteTodo(props.id)">❌</button>
+  </div>
+</template>
